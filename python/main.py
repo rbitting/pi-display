bf606031e285320aad0cf3c426bf2b536d1c7952
Rachel Bitting <rachel.bitting@gmail.com>
2021-12-06 21:21:09 -0500

    Added septa and news stats

--- python/main.py
+++ python/main.py
@@ -8,32 +8,45 @@ from .dictionary import get_word_of_the_day
 from crypto import print_crypto_prices
 from dictionary import print_word_of_the_day
 from network import print_network_speed, print_network_name
-from news import print_news_headlines'''
+'''
+from .pihole import get_pihole_data, PiholeStatus
+from .network import print_network_speed, get_network_name, get_offline_icon, get_online_icon
+from .news import get_news_headlines
+from .septa import get_next_buses, get_bus_icon
 from .util import get_current_date, get_current_date_time
 from .weather import get_weather_data
 from .weather_data import WeatherData
-from .pihole import get_pihole_data, PiholeStatus
-from .network import print_network_speed, get_network_name, get_offline_icon, get_online_icon
-#from septa import print_next_bus
 from PIL import Image, ImageDraw, ImageFont
 
 def get_small_icon(path):
     icon = Image.open(path)
     return icon.resize((icon_size_sm,icon_size_sm));
 
-def print_text_in_box(x, y, text):
-    lines = textwrap.wrap(text, width=35, max_lines=3, placeholder="...")
+def print_md_text_in_box(x, y, text):
+    lines = textwrap.wrap(text, width=30, max_lines=3, placeholder="...")
     for line in lines:
-        height = 24
+        width, height = font_md.getsize(line)
         draw.text((x, y), line, font=font_md, fill=0)
         y += height
+    return y
+    
+def print_sm_text_in_box(x, y, text):
+    lines = textwrap.wrap(text, width=45, max_lines=2, placeholder="...")
+    for line in lines:
+        width, height = font_sm.getsize(line)
+        draw.text((x, y), line, font=font_sm, fill=0)
+        y += height
+    return y
 
 try:
+    print("Initializing display refresh " + get_current_date_time())
+    
     epd = epd5in83_V2.EPD()
     epd.init()
     
     pic = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'python', 'assets', 'lupa.jpg')
-    font_italic = ImageFont.truetype('python/assets/fonts/Roboto-Italic.ttf', 18)
+    font_italic_sm = ImageFont.truetype('python/assets/fonts/Roboto-Italic.ttf', 18)
+    font_italic = ImageFont.truetype('python/assets/fonts/Roboto-Italic.ttf', 22)
     font_sm = ImageFont.truetype('python/assets/fonts/Roboto-Regular.ttf', 16)
     font_md = ImageFont.truetype('python/assets/fonts/Roboto-Regular.ttf', 22)
     font_lg = ImageFont.truetype('python/assets/fonts/Roboto-Regular.ttf', 40)
@@ -43,9 +56,7 @@ try:
     padding = 10
     padding_sm = padding/2
     padding_lg = padding*2
-    col_1_w = 290
-    
-    print("here we go...")
+    col_1_w = 280
     
     Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
     draw = ImageDraw.Draw(Himage)
@@ -62,17 +73,20 @@ try:
     y = 0
     Himage.paste(get_small_icon(network_icon), (x,y))
     x = x + icon_size_sm + padding
+    print('Network name: ' + network_name)
     draw.text((x, y+padding), network_name, font = font_md, fill = 0)
     
     today_x = col_1_w + 30
-    today_y = 60
-    draw.text((today_x, today_y), get_current_date(), font = font_lg, fill = 0)
+    today_y = 45
+    day_and_date = get_current_date().split('\n')
+    draw.text((today_x, today_y), day_and_date[0], font = font_md, fill = 0)
+    draw.text((today_x, today_y+22), day_and_date[1], font = font_lg, fill = 0)
     
     if (weather['enabled']):
         weather = get_weather_data()
-        print("current_icon: " + weather.current_icon)
         if os.path.exists(weather.current_icon):
             Himage.paste(Image.open(weather.current_icon), (0,0))   # Current weather icon
+        print('current temp: ' + weather.current_temp)
         draw.text((60, 0), weather.current_temp, font = font_lg, fill = 0)   # Current temperature
         draw.text((0, 50), weather.current, font = font_md, fill = 0)    # Feels like temp + high/low
         
@@ -101,10 +115,12 @@ try:
     if (pihole['enabled']):
         pihole = get_pihole_data()
         y = 200
+        print(pihole.status)
+        print(pihole.stats)
         draw.text((64, y), pihole.status, font = font_md, fill = 0)    # "Pihole is [enabled/disabled]"
         draw.text((50, y + 30), pihole.stats, font = font_md, fill = 0) 
-        draw.text((52, y + 58), 'requests            total requests', font = font_sm, fill = 0)
-        draw.text((54, y + 74), 'blocked                  blocked', font = font_sm, fill = 0)
+        draw.text((52, y + 58), 'requests           of all requests', font = font_sm, fill = 0)
+        draw.text((54, y + 74), 'blocked                 blocked', font = font_sm, fill = 0)
         
         x = 140
         draw.line((x, y + 40, x, y + 85), fill = 0) # Vertical line break
@@ -112,23 +128,52 @@ try:
         y += 100
         draw.line((0, y, col_1_w, y), fill = 0) # Horizontal line break
     
+    
+    if (septa['enabled']):
+        septa_y = y + padding
+        x = 60
+        bus_routes = get_next_buses()
+        Himage.paste(get_small_icon(get_bus_icon()), (0, septa_y))
+        for bus in bus_routes:
+            y = septa_y
+            draw.text((x, y), bus.route, font = font_italic, fill = 0)
+            arrivals = bus.get_arrivals()
+            for arrival in arrivals:
+                y+=24
+                draw.text((x, y), arrival.eta, font = font_md, fill = 0)
+            x+=75
+        y+=30
+        draw.line((0, y, col_1_w, y), fill = 0) # Horizontal line break
+    
     if (dictionary['enabled']):
         wotd = get_word_of_the_day()
-        y = epd.height - 124
+        y = epd.height - 100
         draw.line((today_x, y-10, epd.width, y-10), fill = 0)
-        draw.text((today_x, y), 'Word of the Day: ' + wotd.word, font = font_md, fill = 0)
-        print_text_in_box(today_x, y+24, wotd.definition)
+        wotd_str = 'Word of the Day: ' + wotd.word
+        print(wotd_str)
+        draw.text((today_x, y), wotd_str, font = font_md, fill = 0)
+        y = print_sm_text_in_box(today_x, y+24, wotd.definition)
     
+    if (news['enabled']):
+        news = get_news_headlines()
+        y = today_y + 80
+        for headline in news:
+            draw.text((today_x, y), '•', font = font_md, fill = 0)
+            y = print_md_text_in_box(today_x + padding, y, headline) + padding_sm
     
-    draw.text((col_1_w + 100, epd.height - 20), 'Last updated ' + get_current_date_time(), font = font_italic, fill = 0)
+    last_updated = 'Last updated ' + get_current_date_time()
+    print(last_updated)
+    draw.text((col_1_w + 100, epd.height - 20), last_updated, font = font_italic_sm, fill = 0)
     
     epd.display(epd.getbuffer(Himage))
 
     #print("Clear...")
-    epd.Clear()
+    #epd.Clear()
     
     print("Go to Sleep...")
     epd.sleep()
+    
+    print("Completed refresh " + get_current_date_time())
 
 except IOError as e:
     print(e)
--- python/septa.py
+++ python/septa.py
@@ -1,21 +1,74 @@
-from config import septa;
-from util import fetch;
+from .config import septa;
+from .util import fetch;
 from datetime import datetime
-import math
+import math 
 
-def print_next_bus():
+class BusArrival():
+    def __init__(self):
+        self.eta = ""
+        self.date = ""
+        
+    @property
+    def eta(self):
+        return self._eta
+        
+    @eta.setter
+    def eta(self, a):
+        self._eta = a
+        
+    @property
+    def date(self):
+        return self._date
+        
+    @date.setter
+    def date(self, a):
+        self._date = a
+        
+        
+        
+class BusStatus():
+    next_arrivals = []
+    
+    def __init__(self):
+        self.route = ""
+        
+    @property
+    def route(self):
+        return self._route
+        
+    @route.setter
+    def route(self, a):
+        self._route = a
+    
+    def get_arrivals(self):
+        return self.next_arrivals
+    
+    def add_arrival(self, a):
+        self.next_arrivals.append(a)
+    
+    def clear_arrivals(self):
+        self.next_arrivals = []
+
+def get_next_buses():
     routes = septa.get('routes')
+    buses = []
     for route in routes:
-       results = get_next_buses(route)
-       route_num = route.get('route')
-       print(route_num)
-       min_range = min([len(results.get(route_num)), septa.get('num')])
-       bus_route = results.get(route_num)
-       for i in range(min_range):
-           print(bus_route[i].get('DateCalender'))
-           print(get_minutes_until_bus(bus_route[i].get('date')))
+        arrival = BusStatus()
+        results = fetch_next_buses(route)
+        route_num = route.get('route')
+        min_range = min([len(results.get(route_num)), septa.get('num')])
+        bus_route = results.get(route_num)
+        arrival.route = route_num
+        arrival.clear_arrivals()
+        for i in range(min_range):
+            bus = BusArrival()
+            bus.date = bus_route[i].get('DateCalender')
+            bus.eta = get_minutes_until_bus(bus_route[i].get('date'))
+            arrival.add_arrival(bus)
+        buses.append(arrival)
+    return buses
 
-def get_next_buses(route):
+def fetch_next_buses(route):
     return fetch('https://www3.septa.org/hackathon/BusSchedules/?req1=' + route.get('stop') + '&req2=' + route.get('route'))
 
 def get_minutes_until_bus(bus_time):
@@ -38,3 +91,6 @@ def get_hours_and_min(minutes):
     hours = math.floor(minutes/60)
     minutes = round(minutes % 60)
     return str(hours) + 'h ' + str(minutes) + 'm'
+    
+def get_bus_icon():
+    return 'python/assets/icons/septa.png'
