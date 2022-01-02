a300dbc6371e6be76237665a9ea005639bf369a8
Rachel Bitting <rachel.bitting@gmail.com>
2022-01-02 14:15:06 -0500

    Show event time if not an all-day event

--- python/google_cal.py
+++ python/google_cal.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import logging
 import pickle
 
@@ -16,52 +16,63 @@ SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 
 def get_gcal_data(gcal_id):
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    token_file = get_absolute_path('python/token.pickle')
-    if path_exists(token_file):
-        with open(token_file, 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            logging.info("Google calendar credentials are invalid. Attempting refresh...")
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(get_absolute_path('python/credentials.json'), SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(get_absolute_path(token_file), 'wb') as token:
-            pickle.dump(creds, token)
-
+    logging.info('Starting gcal fetch...')
     try:
+        """Shows basic usage of the Google Calendar API.
+        Prints the start and name of the next 10 events on the user's calendar.
+        """
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        token_file = get_absolute_path('python/token.pickle')
+        if path_exists(token_file):
+            with open(token_file, 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                logging.info("Google calendar credentials are invalid. Attempting refresh...")
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(get_absolute_path('python/credentials.json'), SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open(get_absolute_path(token_file), 'wb') as token:
+                pickle.dump(creds, token)
+
         service = build('calendar', 'v3', credentials=creds)
 
         # Call the Calendar API
-        now = datetime.datetime.utcnow()
+        now = datetime.utcnow()
         now_str = now.isoformat() + 'Z'  # 'Z' indicates UTC time
         eod = now.replace(day=(now.day+1), hour=0, minute=0, second=0, microsecond=0)
         eod_str = eod.isoformat() + 'Z'
-        logging.info("Google calendar - id: " + gcal_id + ", Start: " + now_str + ", End: " + eod_str)
+        logging.info("Google calendar - Start: " + now_str + ", End: " + eod_str)
         events_result = service.events().list(calendarId=gcal_id, 
-                                              timeMin=now_str, timeMax=eod_str,
-                                              maxResults=1, singleEvents=True,
-                                              orderBy='startTime').execute()
+                                            timeMin=now_str, timeMax=eod_str,
+                                            maxResults=1, singleEvents=True,
+                                            orderBy='startTime').execute()
         events = events_result.get('items', [])
 
         if not events:
             return None
 
-        # Return first event name
-        return events[0]['summary']
+        event = events[0]   # Next upcoming event
+        time = ''
+
+        # Check if event has a start time (all day events only have 'date' key)
+        if 'dateTime' in event['start']:
+            time = datetime.strptime(event['start']['dateTime'], '%Y-%m-%dT%H:%M:%S%z').strftime(' @ %-I:%M%p').lower()
+
+        # Return first event name and time (if applicable)
+        return event['summary'] + time
 
     except HttpError as error:
-        print('An error occurred: %s' % error)
+        logging.exception('Could not get Google Calendar credentials')
+        
+    except BaseException as e:
+        logging.exception(str(e))
 
 def get_gcal_events():
     cal_id = google_cal['cal_id']
@@ -73,5 +84,9 @@ def get_gcal_events():
 
 def print_gcal_event(draw):
     event_name = get_gcal_events()
-    y = COL_2_Y + 65
-    draw.text((COL_2_X, y), event_name, font=FONT_SM, fill=0)
+    if event_name is not None:
+        logging.info('Google calendar result: ' + event_name)
+        y = COL_2_Y + 68
+        draw.text((COL_2_X, y), event_name, font=FONT_SM, fill=0)
+    else:
+        logging.info('No Google Calendar event found.')
